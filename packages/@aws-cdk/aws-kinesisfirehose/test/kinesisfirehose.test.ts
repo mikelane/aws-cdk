560fd1a0da30135ef97605942a6e3e25086f7a5b
mikelane <mikelane@gmail.com>
2020-10-16 00:55:46 +0000

    WIP

--- packages/@aws-cdk/aws-kinesisfirehose/test/kinesisfirehose.test.ts
+++ packages/@aws-cdk/aws-kinesisfirehose/test/kinesisfirehose.test.ts
@@ -1,5 +1,16 @@
 import '@aws-cdk/assert/jest';
 import {} from '../lib';
+import { App, Stack } from '@aws-cdk/core';
+
+let app: App;
+let stack: Stack;
+
+beforeEach(() => {
+    app = new App();
+    stack = new Stack(app, 'Stack', {
+        env: { account: '999999999999', region: 'us-test-1' }
+    })
+})
 
 test('No tests are specified for this package', () => {
   expect(true).toBe(true);
